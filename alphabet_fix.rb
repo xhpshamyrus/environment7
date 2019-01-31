a43b12cb1c8494b74ac4005b4481f2e26a915dfc
xhpshamyrus <46960335+xhpshamyrus@users.noreply.github.com>
2019-01-31 12:53:28 +0500

    incorrect

--- alphabet_fix.rb
+++
@@ -1,8 +0,0 @@
-vowels = %w(a e i o u y)
-alphabet = ('a'..'z')
-vowels_hash = {}
-
-alphabet.each_with_index do |value, index|
-  vowels_hash[value] = index + 1 if vowels.include?(value)
-end
-puts vowels_hash
