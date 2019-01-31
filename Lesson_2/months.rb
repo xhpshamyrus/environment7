1ac0ebd00a260c3a330734732684a10c4ac60d5f
xhpshamyrus <46960335+xhpshamyrus@users.noreply.github.com>
2019-01-31 15:52:53 +0500

    update

---
+++ Lesson_2/months.rb
@@ -0,0 +1,18 @@
+months = {
+  janruary: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31,
+  september: 30,
+  october: 31,
+  november: 30,
+  desember: 31
+}
+
+months.each do |month, day|
+  puts month if day == 30
+end
