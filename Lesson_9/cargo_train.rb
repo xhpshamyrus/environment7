9bc8b7880d8ba49ebdca926a9e1dc9d832f8c2fe
xhpshamyrus <46960335+xhpshamyrus@users.noreply.github.com>
2019-04-01 12:45:59 +0500

    Fixed validate
      validate :number, :presence #corrected
      validate :number, :format, NUMBER_FORMAT #corrected

--- Lesson_9/cargo_train.rb
+++ Lesson_9/cargo_train.rb
@@ -1,7 +1,7 @@
 class CargoTrain < Train
 
-  validate :number, :validate_presence
-  validate :number, :validate_format, NUMBER_FORMAT
+  validate :number, :presence
+  validate :number, :format, NUMBER_FORMAT
 
   def initialize(number)
     super(number)
